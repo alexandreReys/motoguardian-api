a30f8f2b707f86b87ba9e45c543bd6fc6e67ac8a
unknown <anr.alexandre@gmail.com>
2020-12-04 10:16:44 -0300

    .

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const router = require("./router");
+const cors = require("cors");
 
 const app = express();
 
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(cors());
 app.use(router);
 
 const port = process.env.PORT;
