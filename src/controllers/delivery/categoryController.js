82c0d17a17093c5405b2d62b11cd68caebe69ec3
alexandrereys <anr.alexandre@gmail.com>
2021-06-19 20:35:07 -0300

    fix(categoryController)change the product when the category is changed

--- src/controllers/delivery/categoryController.js
+++ src/controllers/delivery/categoryController.js
@@ -1,137 +1,153 @@
 const connection = require("../../mysql-connection");
 
 exports.getAll = function (req, res) {
-  getAll((err, rows) => {
-    if (err) return handleError(err);
-    res.json(rows);
-  });
+    getAll((err, rows) => {
+        if (err) return handleError(err);
+        res.json(rows);
+    });
 };
 
 exports.getAllNotAppList = function (req, res) {
-  getAllNotAppList((err, rows) => {
-    if (err) return handleError(err);
-    res.json(rows);
-  });
+    getAllNotAppList((err, rows) => {
+        if (err) return handleError(err);
+        res.json(rows);
+    });
 };
 
 exports.getSelected = function (req, res) {
-  getSelected((err, rows) => {
-    if (err) return handleError(err);
-    res.json(rows);
-  });
+    getSelected((err, rows) => {
+        if (err) return handleError(err);
+        res.json(rows);
+    });
 };
 
 exports.getByDescription = function (req, res) {
-  getByDescription2(req, (err, rows) => {
-    if (err) return handleError(err);
-    res.json(rows);
-  });
+    getByDescription2(req, (err, rows) => {
+        if (err) return handleError(err);
+        res.json(rows);
+    });
 };
 
 exports.post = function (req, res) {
-  insert(req, (err, rows) => {
-    if (err) return handleError(err);
-    res.json(rows);
-  });
+    insert(req, (err, rows) => {
+        if (err) return handleError(err);
+        res.json(rows);
+    });
 };
 
 exports.put = function (req, res) {
-  update(req, (err, rows) => {
-    if (err) return handleError(err);
-    res.json(rows);
-  });
+    updateCategoryProduct(req, () => {
+        updateCategory(req, (err, rows) => {
+            if (err) return handleError(err);
+            res.json(rows);
+        });
+    });
+
+    function updateCategory(req, callback) {
+        const dados = req.body;
+        const sql = `UPDATE delivery_category 
+                    SET DescriptionCategory = ?
+                    WHERE IdCategory = ?`;
+        const params = [
+            dados.DescriptionCategory,
+            dados.IdCategory,
+        ];
+        connection.query(sql, params, function (err, rows) {
+            return callback(err, rows);
+        });
+    };
+
+    function updateCategoryProduct(req, callback) {
+        const dados = req.body;
+        const sql = `UPDATE produtosvinho  
+                     SET TipoVinho = ?
+                     WHERE TipoVinho = ?`;
+        const params = [
+            dados.DescriptionCategory,
+            dados.PreviousDescriptionCategory,
+        ];
+        connection.query(sql, params, function (err, rows) {
+            return callback();
+        });
+    };
 };
 
 exports.delete = function (req, res) {
-  delete2(req, (err, rows) => {
-    if (err) return handleError(err);
-    res.json(rows);
-  });
+    delete2(req, (err, rows) => {
+        if (err) return handleError(err);
+        res.json(rows);
+    });
 };
 
 ////////////////////////////////////////////////////////////////////////////////////
 
 const handleError = (err) => {
-  if (err.code == "ECONNRESET") {
-    console.log("Erro Query", err.code);
-    res.status(400).send({ message: "ECONNRESET" });
-  } else {
-    throw err;
-  }
+    if (err.code == "ECONNRESET") {
+        console.log("Erro Query", err.code);
+        res.status(400).send({ message: "ECONNRESET" });
+    } else {
+        throw err;
+    }
 };
 
 const getAll = (callback) => {
-  let sql = `SELECT *
+    let sql = `SELECT *
              FROM delivery_category
              ORDER BY DescriptionCategory`;
-  connection.query(sql, function (error, rows) {
-    return callback(error, rows);
-  });
+    connection.query(sql, function (error, rows) {
+        return callback(error, rows);
+    });
 };
 
 const getAllNotAppList = (callback) => {
-  let sql = `SELECT *
+    let sql = `SELECT *
              FROM delivery_category
              WHERE ShowAppListCategory = 0 
              ORDER BY DescriptionCategory`;
-  connection.query(sql, function (error, rows) {
-    return callback(error, rows);
-  });
+    connection.query(sql, function (error, rows) {
+        return callback(error, rows);
+    });
 };
 
 const getSelected = (callback) => {
-  let sql = `SELECT *
+    let sql = `SELECT *
              FROM delivery_category
              WHERE ShowAppSearchCategory <> 0 
              ORDER BY ShowAppSearchCategory desc, DescriptionCategory`;
-  connection.query(sql, function (error, rows) {
-    return callback(error, rows);
-  });
+    connection.query(sql, function (error, rows) {
+        return callback(error, rows);
+    });
 };
 
 const getByDescription2 = (req, callback) => {
-  const stringParam = `%${req.query.DescriptionCategory}%`;
-  const params = [stringParam];
-  let sql = `SELECT *
+    const stringParam = `%${req.query.DescriptionCategory}%`;
+    const params = [stringParam];
+    let sql = `SELECT *
              FROM delivery_category
              WHERE (DescriptionCategory LIKE ?)
              ORDER BY DescriptionCategory`;
-  connection.query(sql, params, function (error, rows) {
-    return callback(error, rows);
-  });
+    connection.query(sql, params, function (error, rows) {
+        return callback(error, rows);
+    });
 };
 
 const insert = (req, callback) => {
-  const dados = req.body;
-  const sql = `INSERT INTO delivery_category ( 
+    const dados = req.body;
+    const sql = `INSERT INTO delivery_category ( 
                 DescriptionCategory 
               ) 
               VALUES ( ? )`;
-  const params = [dados.DescriptionCategory];
-  connection.query(sql, params, function (err, rows) {
-    return callback(err, rows);
-  });
-};
-
-const update = (req, callback) => {
-  const dados = req.body;
-  const sql = `UPDATE delivery_category 
-                SET DescriptionCategory = ?
-                WHERE IdCategory = ?`;
-  const params = [
-    dados.DescriptionCategory,
-    dados.IdCategory,
-  ];
-  connection.query(sql, params, function (err, rows) {
-    return callback(err, rows);
-  });
+    const params = [dados.DescriptionCategory];
+    connection.query(sql, params, function (err, rows) {
+        return callback(err, rows);
+    });
 };
 
 const delete2 = (req, callback) => {
-  const { IdCategory } = req.query;
-  const sql = "DELETE FROM delivery_category WHERE IdCategory = ?";
-  const params = [IdCategory];
-  connection.query(sql, params, function (err, rows) {
-    return callback(err, rows);
-  });
+    const { IdCategory } = req.query;
+    const sql = "DELETE FROM delivery_category WHERE IdCategory = ?";
+    const params = [IdCategory];
+    connection.query(sql, params, function (err, rows) {
+        return callback(err, rows);
+    });
 };
